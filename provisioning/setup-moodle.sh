3bc3248130251b39af9ddbaa86094ad11b7c931f
Julius <j.hoedjes@protonmail.com>
2018-11-06 15:21:31 +0100

    updated version, unattended installation and redis config

--- provisioning/setup-moodle.sh
+++ provisioning/setup-moodle.sh
@@ -6,7 +6,7 @@ chmod 777 /srv/webdata/moodledata
 chown -R apache: /srv/*
 
 # get moodle
-version="MOODLE_33_STABLE"
+version="MOODLE_35_STABLE"
 echo " "
 echo "installing $version"
 echo " "
@@ -15,7 +15,19 @@ su -s /bin/bash -c "git clone -b $version git://git.moodle.org/moodle.git ." apa
 cd
 
 # install moodle as apache
-su -s /bin/bash -c "/opt/rh/rh-php71/root/bin/php /srv/webdata/www/admin/cli/install.php --lang=en --wwwroot=http://localhost:8383 --dataroot=/srv/webdata/moodledata --dbtype=mysqli --dbhost=data-server.example.com --dbname=moodle --dbuser=moodleuser --dbpass=yourpassword --dbport=3306 --fullname=test-moodle --shortname=tst-mdl --adminuser=admin --adminpass=AdminAdmin123! --adminemail=root@localhost.com --non-interactive --agree-license" apache
+su -s /bin/bash -c "/opt/rh/rh-php71/root/bin/php /srv/webdata/www/admin/cli/install.php --lang=en --wwwroot=http://localhost:8383 --dataroot=/srv/webdata/moodledata --dbtype=mariadb --dbhost=data-server.example.com --dbname=moodle --dbuser=moodleuser --dbpass=yourpassword --dbport=3306 --fullname=test-moodle --shortname=tst-mdl --adminuser=admin --adminpass=AdminAdmin123! --adminemail=root@localhost.com --non-interactive --agree-license" apache
+
+# add redis config for Moodle
+cat <<EOF >> /srv/webdata/www/config.php
+//REDIS
+$CFG->session_handler_class = '\core\session\redis';
+$CFG->session_redis_host = '127.0.0.1';
+$CFG->session_redis_port = 6379;  // Optional.
+$CFG->session_redis_database = 0;  // Optional, default is db 0.
+$CFG->session_redis_prefix = ''; // Optional, default is don't set one.
+$CFG->session_redis_acquire_lock_timeout = 120;
+$CFG->session_redis_lock_expire = 7200;
+EOF
 
 # copy info pages for debugging
 cp /vagrant/provisioning/files/hostname.php /srv/webdata/www/hostname.php
@@ -23,9 +35,12 @@ cp /vagrant/provisioning/files/phpinfo.php /srv/webdata/www/phpinfo.php
 chown apache: /srv/webdata/www/hostname.php
 chown apache: /srv/webdata/www/phpinfo.php
 
+# restart apache
+systemctl restart httpd.service
+
 echo " "
 echo "url: http://localhost:8383"
 echo "username: admin"
 echo "password: AdminAdmin123!"
 echo " "
-echo "Don't forget to configure Redis cache"
+echo "Don't forget to configure Redis cache in the UI"
