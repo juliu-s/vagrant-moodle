50bcde701be71de4cedbb7a2061025b838c8be32
Julius <j.hoedjes@protonmail.com>
2018-11-06 15:20:45 +0100

    added packages and vim config

--- provisioning/common.sh
+++ provisioning/common.sh
@@ -12,7 +12,28 @@ yum -y install tree \
     vim \
     bind-utils \
     git \
+    iotop \
+    iftop \
     tracer
+
+# create direcotries for vim stuff
+mkdir -p ~/.vim/bundle
+mkdir ~/.vim/autoload
+mkdir ~/.vim/colors
+
+# install pathogen
+curl -so ~/.vim/autoload/pathogen.vim https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+# install plugins
+git clone -q https://github.com/itchyny/lightline.vim ~/.vim/bundle/lightline.vim
+git clone -q https://github.com/sheerun/vim-polyglot.git ~/.vim/bundle/vim-polyglot
+# install color scheme
+curl -so ~/.vim/colors/sahara.vim https://sanctum.geek.nz/cgit/vim-sahara.git/plain/colors/sahara.vim
+# install .vimrc
+curl -so ~/.vimrc https://gitlab.com/juliu-s/install-arch/raw/master/configs/vimrc
+
+# setup vim stuff for vagrant user
+rsync -qa /root/.vi* /home/vagrant/ --exclude=".git"
+chown -R vagrant: /home/vagrant
 
 # append hosts to /etc/hosts
 echo "192.168.100.100   data-server.example.com data-server" >> /etc/hosts
