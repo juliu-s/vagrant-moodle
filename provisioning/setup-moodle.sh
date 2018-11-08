322eaee822452801d7816577ad9a207f5f7934c1
Julius <j.hoedjes@protonmail.com>
2018-11-08 11:15:25 +0100

    added benchmark plugin and local cache dir

--- provisioning/setup-moodle.sh
+++ provisioning/setup-moodle.sh
@@ -17,6 +17,13 @@ cd
 # install moodle as apache
 su -s /bin/bash -c "/opt/rh/rh-php71/root/bin/php /srv/webdata/www/admin/cli/install.php --lang=en --wwwroot=http://localhost:8383 --dataroot=/srv/webdata/moodledata --dbtype=mariadb --dbhost=data-server.example.com --dbname=moodle --dbuser=moodleuser --dbpass=yourpassword --dbport=3306 --fullname=test-moodle --shortname=tst-mdl --adminuser=admin --adminpass=AdminAdmin123! --adminemail=root@localhost.com --non-interactive --agree-license" apache
 
+# install benchmark plugin
+cd /srv/webdata/www/report
+su -s /bin/bash -c "git clone -q https://github.com/mikasmart/benchmark benchmark" apache
+cd
+
+su -s /bin/bash -c "/opt/rh/rh-php71/root/bin/php /srv/webdata/www/admin/cli/upgrade.php --non-interactive" apache
+
 # add redis config for Moodle
 cat <<EOF >> /srv/webdata/www/config.php
 //REDIS
@@ -29,14 +36,19 @@ cat <<EOF >> /srv/webdata/www/config.php
 \$CFG->session_redis_lock_expire = 7200;
 EOF
 
+# add local cache dir (nfs is slow)
+echo -e "\n// Intended for local node caching." >> /srv/webdata/www/config.php
+echo "\$CFG->localcachedir = '/tmp/moodle_temp_dir';    // Intended for local node caching." >> /srv/webdata/www/config.php
+
 # copy info pages for debugging
 cp /vagrant/provisioning/files/hostname.php /srv/webdata/www/hostname.php
 cp /vagrant/provisioning/files/phpinfo.php /srv/webdata/www/phpinfo.php
 chown apache: /srv/webdata/www/hostname.php
 chown apache: /srv/webdata/www/phpinfo.php
 
-# restart apache
+# restart php & apache
 systemctl restart httpd.service
+systemctl restart rh-php71-php-fpm.service
 
 echo " "
 echo "url: http://localhost:8383"
