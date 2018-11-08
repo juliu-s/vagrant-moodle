e1523f850c15f408fe2f13d43e0bcd97ee2f582c
Julius <j.hoedjes@protonmail.com>
2018-11-08 11:14:52 +0100

    removed rpcbind, added mysqltuner

--- provisioning/data-server.sh
+++ provisioning/data-server.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-# install packages & start service
+# install packages, install config & start service
 yum -y install mariadb-server
 
 cp /vagrant/provisioning/files/0_moodle.cnf /etc/my.cnf.d/0_moodle.cnf
+
+touch /var/lib/mysql/data-server.example.com.err
+chown mysql: /var/lib/mysql/data-server.example.com.err
 
 systemctl enable mariadb.service
 systemctl start mariadb.service
@@ -14,12 +17,17 @@ mysql -e "GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,CREATE TEMPORARY TABLES,DROP,
 mysql -e "GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,CREATE TEMPORARY TABLES,DROP,INDEX,ALTER ON moodle.* TO 'moodleuser'@'web1.example.com' IDENTIFIED BY 'yourpassword';"
 mysql -e "GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,CREATE TEMPORARY TABLES,DROP,INDEX,ALTER ON moodle.* TO 'moodleuser'@'web2.example.com' IDENTIFIED BY 'yourpassword';"
 
-# nfs
+# install mysqltuner
+cd /home/vagrant
+git clone -q https://github.com/major/MySQLTuner-perl mysqltuner
+cd
+chown -R vagrant: /home/vagrant
+
+# export nfs
 mkdir /srv/webexport
 echo "/srv/webexport web1.example.com(rw,no_root_squash) web2.example.com(rw,no_root_squash)" >> /etc/exports
 exportfs -avr
 
-systemctl enable rpcbind
-systemctl start rpcbind
+# start nfs-server
 systemctl enable nfs-server
 systemctl start nfs-server
