2fe63d7cee7acf26975cfb8a65546bc17014b501
Julius <j.hoedjes@protonmail.com>
2018-11-23 14:53:31 +0100

    increase haproxy logging

--- provisioning/lb.sh
+++ provisioning/lb.sh
@@ -7,6 +7,14 @@ yum -y install haproxy \
 # copy config
 cp /vagrant/provisioning/files/haproxy.cfg /etc/haproxy/haproxy.cfg
 
+# setup rsyslog for haproxy
+sed -i 's/#$ModLoad imudp/$ModLoad imudp/g' /etc/rsyslog.conf
+sed -i 's/#$UDPServerRun 514/$UDPServerRun 514/g' /etc/rsyslog.conf
+echo "local2.*    /var/log/haproxy.log" > /etc/rsyslog.d/haproxy.conf
+
+# restart rsyslog
+systemctl restart rsyslog.service
+
 # start & enable haproxy
 systemctl enable haproxy.service
 systemctl start haproxy.service
