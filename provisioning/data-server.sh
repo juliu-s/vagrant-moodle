136ef53f2039f37e48146aae04faa20b955b84c5
Julius <j.hoedjes@protonmail.com>
2018-12-13 15:46:18 +0100

    added grafana + config

--- provisioning/data-server.sh
+++ provisioning/data-server.sh
@@ -1,9 +1,72 @@
 #!/bin/bash
 
+# add grafana repo
+cat <<EOF >> /etc/yum.repos.d/grafana.repo
+[grafana]
+name=grafana
+baseurl=https://packagecloud.io/grafana/stable/el/7/\$basearch
+repo_gpgcheck=1
+enabled=1
+gpgcheck=1
+gpgkey=https://packagecloud.io/gpg.key https://grafanarel.s3.amazonaws.com/RPM-GPG-KEY-grafana
+sslverify=1
+sslcacert=/etc/pki/tls/certs/ca-bundle.crt
+EOF
+
+# update yum cache
+yum makecache fast -y
+
 # install packages, install config & start service
 yum -y install mariadb-server \
-    redis
+    redis \
+    influxdb \
+    grafana
 
+# configure influxdb
+sed -i 's/#\ bind-address\ =\ ":8086"/bind-address\ =\ "192\.168\.100\.100:8086"/g' /etc/influxdb/influxdb.conf
+sed -i 's/#\ auth-enabled\ =\ false/auth-enabled\ =\ true/g' /etc/influxdb/influxdb.conf
+
+# start and enable influxdb
+systemctl enable influxdb.service
+systemctl start influxdb.service
+
+# create influxdb user
+sleep 1
+curl -XPOST "http://192.168.100.100:8086/query" --data-urlencode "q=CREATE USER username WITH PASSWORD 'password' WITH ALL PRIVILEGES"
+
+# configure grafana
+mkdir /etc/grafana/provisioning/templates
+chown root:grafana /etc/grafana/provisioning/templates
+sed -i 's/;admin_user\ =\ admin/admin_user\ =\ admin/g' /etc/grafana/grafana.ini
+sed -i 's/;admin_password\ =\ admin/admin_password\ =\ admin123/g' /etc/grafana/grafana.ini
+
+# import data sources
+cp /vagrant/provisioning/files/telegraf.yaml /etc/grafana/provisioning/datasources/telegraf.yaml
+
+# import dashboard source
+cp /vagrant/provisioning/files/grafana_dasboards.yaml /etc/grafana/provisioning/dashboards/grafana.yaml
+
+# copy dashboards files from template
+cp /vagrant/provisioning/files/grafana_basic_stats_dashboard_template.json /etc/grafana/provisioning/templates/grafana_basic_stats_dashboard.json
+cp /vagrant/provisioning/files/grafana_mariadb_stats_dashboard_template.json /etc/grafana/provisioning/templates/grafana_mariadb_stats_dashboard.json
+# haproxy
+# ...
+
+# update templates for basic stats:
+sed -i 's/${DS_INFLUXDB}/telegraf/g' /etc/grafana/provisioning/templates/grafana_basic_stats_dashboard.json
+sed -i 's/"title":\ "Telegraf\ -\ system\ metrics"/"title":\ "Basic stats"/g' /etc/grafana/provisioning/templates/grafana_basic_stats_dashboard.json
+
+# update templates for mariadb stats:
+sed -i 's/${DS_INFLUXDB}/telegraf/g' /etc/grafana/provisioning/templates/grafana_mariadb_stats_dashboard.json
+sed -i 's/"title":\ "Service\ -\ MySQL\ Metrics"/"title":\ "MariaDB\ stats"/g' /etc/grafana/provisioning/templates/grafana_mariadb_stats_dashboard.json
+
+# fix permissions
+chown -R root:grafana /etc/grafana/provisioning
+
+# add telegraf config for mariadb
+cp /vagrant/provisioning/files/telegraf_mariadb.conf /etc/telegraf/telegraf.d/telegraf_mariadb.conf
+
+# copy mysql config
 cp /vagrant/provisioning/files/0_moodle.cnf /etc/my.cnf.d/0_moodle.cnf
 
 touch /var/log/mariadb/mariadb-error.log
@@ -12,6 +75,9 @@ chown mysql: /var/log/mariadb/*
 
 systemctl enable mariadb.service
 systemctl start mariadb.service
+
+# start collecting mariadb stats
+systemctl restart telegraf.service
 
 # create database
 mysql -e "CREATE DATABASE moodle DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
@@ -37,7 +103,6 @@ sed -i 's/# maxmemory <bytes>/maxmemory 512M/g' /etc/redis.conf
 sed -i 's/appendonly no/appendonly yes/g' /etc/redis.conf
 sed -i 's/loglevel notice/loglevel debug/g' /etc/redis.conf
 
-
 # create script & copy unit file to to activate changes for redis requirements
 cat <<EOF >> /usr/local/bin/redis_req.sh
 #!/bin/sh
@@ -57,3 +122,5 @@ systemctl enable redis_rq.service
 systemctl start redis_rq.service
 systemctl enable redis.service
 systemctl start redis.service
+systemctl start grafana-server
+systemctl enable grafana-server
